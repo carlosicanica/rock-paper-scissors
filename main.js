86c9675c9a872e86a5cc96535b88158f95a88d0e
carlosicanica <carlosicanica@gmail.com>
2023-04-18 14:14:40 +0200

    create getComputerChoice funciton

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-function getComputerChoice () {
 
+
+function getComputerChoice () {
+    let options = ["rock", "paper", "scissors"];
+    return options = [Math.floor(Math.random()* options.length)];
 }
+
+console.log(getComputerChoice());
