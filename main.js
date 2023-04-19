abb76464edd944f490c9f694a4b8d4bb1da8e1b2
carlosicanica <carlosicanica@gmail.com>
2023-04-19 18:54:28 +0200

    add if statements to return the outcome

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 
 
 function getComputerChoice () {
-    let options = ["rock", "paper", "scissors"];
+    const compOptions = ["rock", "paper", "scissors"];
     let randomNumber = Math.floor(Math.random() * 3);
-    let choice = options[randomNumber];
+    let choice = compOptions[randomNumber];
     return choice;
 }
 
 console.log(getComputerChoice());
 
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        return "It's a tie!";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lost!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You won!";
+    }  else if (playerSelection === "paper" && computerSelection === "paper") {
+        return "It's a tie!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lost!";
+    }  else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You won!";
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        return "It's a tie!";
+    }  else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lost!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You won!";
+    }
+}
+
+// playRound();
