6c691c10566e2b75d3f394d45ad16c89aac97e4a
carlosicanica <carlosicanica@gmail.com>
2023-04-19 18:22:39 +0200

    funciton to generate computer choice

--- main.js
+++ main.js
@@ -2,20 +2,10 @@
 
 function getComputerChoice () {
     let options = ["rock", "paper", "scissors"];
-    return options = [Math.floor(Math.random()* options.length)];
-} if (getComputerChoice() == 0) {
-    // return "rock";
-    console.log("rock");
-} else if (getComputerChoice() == 1) {
-    // return "paper";
-    console.log("paper");
-} else if (getComputerChoice() == 2) {
-    // return "scissors";
-    console.log("scissors");
-} else {
-    console.log("din mamma");
+    let randomNumber = Math.floor(Math.random() * 3);
+    let choice = options[randomNumber];
+    return choice;
 }
 
-
-// console.log(getComputerChoice());
+console.log(getComputerChoice());
 
