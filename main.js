962f01822dcb834802ac7e187d8dbbb73406093c
carlosicanica <carlosicanica@gmail.com>
2023-04-19 18:17:43 +0200

    changes

--- main.js
+++ main.js
@@ -3,6 +3,19 @@
 function getComputerChoice () {
     let options = ["rock", "paper", "scissors"];
     return options = [Math.floor(Math.random()* options.length)];
+} if (getComputerChoice() == 0) {
+    // return "rock";
+    console.log("rock");
+} else if (getComputerChoice() == 1) {
+    // return "paper";
+    console.log("paper");
+} else if (getComputerChoice() == 2) {
+    // return "scissors";
+    console.log("scissors");
+} else {
+    console.log("din mamma");
 }
 
-console.log(getComputerChoice());
+
+// console.log(getComputerChoice());
+
