b27667189bdc6050e37b2a4d87ee0078f1daee1d
carlosicanica <carlosicanica@gmail.com>
2023-04-19 19:01:18 +0200

    add output messages of round result

--- main.js
+++ main.js
@@ -7,28 +7,32 @@ function getComputerChoice () {
     return choice;
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "rock") {
-        return "It's a tie!";
+        return "It's a tie! You both chose rock.";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lost!";
+        return "You lost! Computer chose paper.";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You won!";
+        return "You won! Computer chose scissors.";
     }  else if (playerSelection === "paper" && computerSelection === "paper") {
-        return "It's a tie!";
+        return "It's a tie! You both chose paper.";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lost!";
+        return "You lost! Computer chose scissors.";
     }  else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You won!";
+        return "You won! Computer chose rock.";
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        return "It's a tie!";
+        return "It's a tie! You both chose scissors.";
     }  else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lost!";
+        return "You lost! Computer chose rock.";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You won!";
+        return "You won! Computer chose paper.";
     }
 }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
 // playRound();
